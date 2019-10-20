a12aaa0cb74c4514ec7a71b07d230aec0a27fa76
Megan Wong <mwong068@gmail.com>
2019-10-20 01:25:36 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -26,13 +26,13 @@ def monopoly_with_third_tier
   {
 	    :railroads => {
 	      :pieces => 4,
+	      :names => ["Margie", "Maggie"],
 	      :rent_in_dollars => {
 	        :renters => ["Margaret", "Jessica", "Lorraine"],
 	        :rento => 2,
 	        :rented => 3,
 	        :renter => 4
 	      }
-	      :names => ["Margie", "Maggie"]
 	    }
 	}
 end
